a632e2deaef64a061cdc7ef92deb94650c1c8034
jorgebarrero <jorgebarrero@gmail.com>
2020-04-02 09:42:33 -0400

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { AuthGuard } from "src/shared";
+
+const routes: Routes = [
+    {
+        path: "",
+        loadChildren: () =>
+            import("./layout/layout.module").then(m => m.LayoutModule),
+        canActivate: [AuthGuard]
+    },
+    {
+        path: "login",
+        loadChildren: () =>
+            import("./pages/login/login.module").then(m => m.LoginModule)
+    },
+    {
+        path: "signup",
+        loadChildren: () =>
+            import("./pages/signup/signup.module").then(m => m.SignupModule)
+    },
+    {
+        path: "error",
+        loadChildren: () =>
+            import("./pages/server-error/server-error.module").then(
+                m => m.ServerErrorModule
+            )
+    },
+    {
+        path: "access-denied",
+        loadChildren: () =>
+            import("./pages/access-denied/access-denied.module").then(
+                m => m.AccessDeniedModule
+            )
+    },
+    {
+        path: "not-found",
+        loadChildren: () =>
+            import("./pages/not-found/not-found.module").then(
+                m => m.NotFoundModule
+            )
+    },
+    { path: "**", redirectTo: "not-found" }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+// Angular
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+// Vendor
+import { LanguageTranslationModule } from 'src/shared/modules/language-translation/language-translation.module'
+
+// Shared
+import { AuthGuard } from 'src/shared';
+
+
+// Local
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        LanguageTranslationModule,
+        AppRoutingModule
+    ],
+    declarations: [AppComponent],
+    providers: [AuthGuard],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
